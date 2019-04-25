29cd0d9374f141b3d42cef48014fabe10f26a889
Sean Carter <scarter0313@gmail.com>
2019-04-25 03:32:23 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -24,10 +24,3 @@ def find_the_cheese(has_cheese)
 end
 end
 
-# def find_the_cheese(potentially_cheesy_items)
-#   cheeses = %w(gouda cheddar camembert)
-
-#   potentially_cheesy_items.find do |maybe_cheese|
-#     cheeses.include?(maybe_cheese)
-#   end
-# end
