f57eea30379c247a4e3bd79e6fe6c99a1dfa6cb8
Sean Carter <scarter0313@gmail.com>
2019-04-25 03:09:17 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,6 +1,6 @@
 def roll_call_dwarves(dwarves)
-  dwarves.each_with_index do |dwarf, index|
-    puts "#{index + 1}. #{dwarf}"
+  dwarves.each_with_index do |dwarves, index|
+    
 end
 end
 def summon_captain_planet
