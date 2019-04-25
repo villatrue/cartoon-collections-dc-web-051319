fb940b85e5e8199ccc74c0f9eefedc82c49a49d0
Sean Carter <scarter0313@gmail.com>
2019-04-25 03:14:24 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -5,7 +5,8 @@ end
 end
 
 def summon_captain_planet(planateer_calls)
-  planateer_calls.collect {|calls| calls.capitalize + !""
+  planateer_calls.collect {|calls| calls.capitalize + "!"}
+end
 end
 
 def long_planeteer_calls
